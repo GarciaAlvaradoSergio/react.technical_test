c909215b5abb05ce3b726cebf373fdb091b8ac1a
Sergio Garcia <al222110820@gmail.com>
2025-05-23 12:00:37 -0600

    config, env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import environment from 'vite-plugin-environment'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    environment(['VITE_API_URL'])
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
